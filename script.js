8af473b94284805dfe55992c4065ba080abc0cac
Xanthe Sharp <xsharp@virginmedia.com>
2021-07-09 11:31:41 +0100

    add examples of hoisting and temporal dead zone

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
-function calcAge(birthYear) {
+
+/*function calcAge(birthYear) {
   const age = 2021 - birthYear;
 
   function printAge() {
@@ -31,4 +32,44 @@ function calcAge(birthYear) {
 }
 
 const firstName = 'Xanthe';
-calcAge(1982); //can have dif variables with the same name, in different scopes. Also can have dif functions with the same parameter names. Each param is only defined in the scope of that function.
+calcAge(1982); //can have dif variables with the same name, in different scopes. Also can have dif functions with the same parameter names. Each param is only defined in the scope of that function. */
+
+//Hoisting with variables
+//console.log(me); // var - hoisted to value 'undefined'
+//console.log(job); // let - can't access before initialization. In TDZ.
+//console.log(year); // Same error, in TDZ
+
+var me = 'Xanthe';
+let job = 'mum';
+const year = 1982;
+
+//Hoisting with functions
+console.log(addDecl(2, 3)); // prints 5. Can call function declaration before it's defined.
+//console.log(addExpr(2, 3)); // can't access before initialiized.
+//console.log(addArrow(2, 3)); // var, error states not a function because undefined
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart(); //function runs because at this point, numProducts is undefiend. So don't use var. Declare variables at top of each scope. Always declare functions first, use after declaration.
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x); //true. Creates property on global window object.
+console.log(y === window.x); //false
+console.log(z === window.x); //false
